d146c77d7c333b9750882bd28cac5f6422986812
Kitsana Poonsri <Kitsana.p2547@gmail.com>
2026-06-01 14:48:49 +0700

    fix google sheet submit

--- udefined-creative-hub-main/src/pages/Register.tsx
+++ udefined-creative-hub-main/src/pages/Register.tsx
@@ -79,14 +79,13 @@ const Register = () => {
 
   try {
     await fetch(GOOGLE_SCRIPT_URL, {
-      method: "POST",
-      mode: "no-cors",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload),
-    });
-
+  method: "POST",
+  mode: "no-cors",
+  headers: {
+    "Content-Type": "text/plain;charset=utf-8",
+  },
+  body: JSON.stringify(payload),
+});
     setSubmitted(true);
 
     toast({
